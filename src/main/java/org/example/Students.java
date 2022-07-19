e02d7c54923d6ec6ba28752fcd0547cef6b6aaef
David <david.rtgr@gmail.com>
2022-07-19 17:50:26 +0300

    first commit

---
+++ src/main/java/org/example/Students.java
@@ -0,0 +1,69 @@
+package org.example;
+
+public class Students {
+    String fullName;
+    String universityId;
+    int currentCourseNumber;
+    float avgExamScore;
+
+    public Students(String fullName, String universityId, int currentCourseNumber, float avgExamScore) {
+        this.fullName = fullName;
+        this.universityId = universityId;
+        this.currentCourseNumber = currentCourseNumber;
+        this.avgExamScore = avgExamScore;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUniversityId() {
+        return universityId;
+    }
+
+    public void setUniversityId(String universityId) {
+        this.universityId = universityId;
+    }
+
+    public int getCurrentCourseNumber() {
+        return currentCourseNumber;
+    }
+
+    public void setCurrentCourseNumber(int currentCourseNumber) {
+        this.currentCourseNumber = currentCourseNumber;
+    }
+
+    public float getAvgExamScore() {
+        return avgExamScore;
+    }
+
+    public void setAvgExamScore(float avgExamScore) {
+        this.avgExamScore = avgExamScore;
+    }
+
+    enum StudyProfile{
+        MEDICINE("Медицина"), ENGINEERING("Инженерия"), MARKETING("Маркетинг");
+        private String profileName;
+        StudyProfile(String profileName){
+            this.profileName = profileName;
+        }
+        public String getProfileName(){
+            return profileName;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Students{" +
+                "fullName='" + fullName + '\'' +
+                ", universityId='" + universityId + '\'' +
+                ", currentCourseNumber=" + currentCourseNumber +
+                ", avgExamScore=" + avgExamScore +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/University.java
@@ -0,0 +1,69 @@
+package org.example;
+
+public class University {
+
+    String id;
+    String fullName;
+    String shortName;
+    int yearOfFoundation;
+    Students.StudyProfile mainProfile;
+
+    public University(String id, String fullName, String shortName, int yearOfFoundation, Students.StudyProfile mainProfile) {
+        this.id = id;
+        this.fullName = fullName;
+        this.shortName = shortName;
+        this.yearOfFoundation = yearOfFoundation;
+        this.mainProfile = mainProfile;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public int getYearOfFoundation() {
+        return yearOfFoundation;
+    }
+
+    public void setYearOfFoundation(int yearOfFoundation) {
+        this.yearOfFoundation = yearOfFoundation;
+    }
+
+    public Students.StudyProfile getMainProfile() {
+        return mainProfile;
+    }
+
+    public void setMainProfile(Students.StudyProfile mainProfile) {
+        this.mainProfile = mainProfile;
+    }
+
+    @Override
+    public String toString() {
+        return "University{" +
+                "id='" + id + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", shortName='" + shortName + '\'' +
+                ", yearOfFoundation=" + yearOfFoundation +
+                ", mainProfile=" + mainProfile +
+                '}';
+    }
+}
