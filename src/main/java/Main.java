335ae47acec2fae86c96441e681cc66c36a6cb08
David <david.rtgr@gmail.com>
2022-07-31 17:25:32 +0100

    a few changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import XLSXReader.Reader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String filePath = "C:\\\\Users\\\\user\\\\IdeaProjects\\\\Students_University\\\\src\\\\main\\\\resources\\\\universityInfo.xlsx";
+        String filePath = "C:\\Users\\User\\IdeaProjects\\SkillFactory\\src\\main\\resources\\universityInfo.xlsx";
 
         System.out.println(Reader.StudentReader(filePath));
         System.out.println();
